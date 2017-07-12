59f10cdc8011c6bd8356ee33639d2070c64a4684
MuhammadSamiUllahKhan <muhammadsami786007@outlook.com>
2017-07-13 02:25:49 +0500

    1st Commit

---
+++ TestProjectForIdentity/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestProjectForIdentity.Startup))]
+namespace TestProjectForIdentity
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
